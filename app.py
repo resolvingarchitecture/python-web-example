099d7df1dac82331f09cb71dba7a62c1e9c4df30
objectorange <objectorange@1m5.io>
2025-11-03 19:08:56 -0500

    changed cookie to session adding apis

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask import jsonify
 from flask import make_response
 from flask import redirect
 from flask import request
 from flask import render_template
+from flask import session
 from flask import url_for
 from markupsafe import escape
 
 app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret. TODO: load it from the environment
+app.secret_key = b'_f9j38hfnu746bnakdk'
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -14,11 +19,36 @@ def page_not_found(error):
 
 @app.route("/")
 def index():
-    username = request.cookies.get('username')
-    if username:
-        return render_template('home.html', username=username)
+    if "username" in session:
+        return render_template('index.html', username=session["username"])
     else:
         return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            log_the_user_in(request.form['username'])
+            return redirect(url_for('index'))
+        else:
+            app.logger.warning('Invalid login attempt',
+                               request.form['username'],
+                               request.form['password'])
+            return render_template('login.html', error='Invalid username/password')
+    else:
+        return render_template('login.html', error=None)
+
+def valid_login(username, password):
+    return (username=='User' and password=='123') or (username=='Admin' and password=='123')
+
+def log_the_user_in(username):
+     session["username"] = username
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop("username")
+    return redirect('login')
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
@@ -53,30 +83,14 @@ def projects():
 def about():
     return 'The about page'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            return render_template('login.html', error='Invalid username/password')
-    else:
-        return render_template('login.html', error=None)
+# APIs
+@app.route('/api/users', methods=['GET'])
+def api_users():
+    users = get_all_users()
+    return jsonify(users)
 
-def valid_login(username, password):
-    return username=='User' and password=='123'
-
-def log_the_user_in(username):
-     resp = make_response(render_template('home.html', username=username))
-     resp.set_cookie('username', username)
-     return resp
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    resp = make_response(render_template('login.html'))
-    resp.delete_cookie('username')
-    return resp
+def get_all_users():
+    return ['User','Admin']
 
 if __name__ == '__main__':
     app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
